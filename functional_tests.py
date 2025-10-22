d923668559afec4048bbb1a6d74c6b234ec96532
Nathan Harris <nharrisanalyst@gmail.com>
2025-10-22 10:05:13 -0700

    FIRST FT now uses unittest

---
+++ functional_tests.py
@@ -0,0 +1,24 @@
+import unittest
+from selenium import webdriver
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get('http://www.localhost:8000')
+        
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-DO", self.browser.title)
+    
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+        # Satisfied, she goes back to sleep
+        
+if __name__ == "__main__":  
+    unittest.main()
