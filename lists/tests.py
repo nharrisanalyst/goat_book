55932968d17590574f2ee259ef5bea102acd8596
Nathan Harris <nharrisanalyst@gmail.com>
2025-10-22 17:03:50 -0700

    Lists app added, First List added and Lists View function

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
     def test_bad_math(self):
-        self.assertEqual(1+1,3)
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.endswith('</html>'))
