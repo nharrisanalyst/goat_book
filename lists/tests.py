3504f2ae1a9043d17d0d76cdbff453cebd59571a
Nathan Harris <nharrisanalyst@gmail.com>
2025-10-22 17:46:36 -0700

    added "" url to superlists url.py and cleaned up test

--- lists/tests.py
+++ lists/tests.py
@@ -4,10 +4,9 @@ from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_bad_math(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.startswith('<html>'))
-        self.assertTrue(html.endswith('</html>'))
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response,'<title>To-Do lists</title>')
+        self.assertContains(response,'<html>')
+        self.assertContains(response,'</html>')
+        
