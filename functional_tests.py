950622a00c99a62c737204077ed7d21037884922
Nathan Harris <nharrisanalyst@gmail.com>
2025-10-24 17:49:14 -0700

    Updated functionak test

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
+    def check_for_row_in_list_table(self,text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = self.browser.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(text, [row.text for row in rows])
+    
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -32,29 +37,21 @@ class NewVisitorTest(unittest.TestCase):
         
         # When she hits enter the page updates and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
         
-        table = self.browser.find_element(By.ID,"id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows]
-        )
-        
-        self.assertIn(
-            "1: Buy peacock feathers", 
-            [row.text for row in rows]
-            )
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         
-        # The page updates again, and now shows both items on her list
-        [...]
-        
+        # the page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+       
 if __name__ == "__main__":  
     unittest.main()
