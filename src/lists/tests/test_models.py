7b7129448d6b1456b5f3395571c72d1fe193a1a8
Nathan Harris <nharrisanalyst@gmail.com>
2025-12-02 14:12:42 -0800

    Implement duplicate item validation at model layer

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -2,29 +2,19 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 from lists.models import Item, List   
+
+class ItemModelTest(TestCase):
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, "")
         
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
+    def test_item_is_related_to_list(self):
+        mylist = List.objects.create()
+        item = Item()
+        item.list = mylist
+        item.save()
+        self.assertIn(item, mylist.item_set.all())
         
-        first_item= Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = mylist
-        first_item.save()
-        
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = mylist
-        second_item.save()
-        
-        saved_items = Item.objects.all()
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item") 
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, mylist)
         
     def test_cannot_save_null_list_items(self):
         mylist = List.objects.create()
@@ -37,7 +27,26 @@ class ListAndItemModelTest(TestCase):
         item = Item(list=mylist, text="")  
         with self.assertRaises(ValidationError):
             item.full_clean()
+        
+    def test_duplicate_items_are_invalid(self):
+        mylist = List.objects.create()
+        Item.objects.create(list=mylist, text="bla")
+        with self.assertRaises(ValidationError):
+            item = Item(list=mylist, text="bla")
+            item.full_clean()
             
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text="bla")
+        item2 = Item.objects.create(list=list2, text="bla")
+        item2.full_clean() # should not raise
+
+
+class ListModelTest(TestCase):
     def test_get_absolute_url(self):
         mylist = List.objects.create()
         self.assertEqual(mylist.get_absolute_url(), f"/lists/{mylist.id}/")
+
+    
+        
